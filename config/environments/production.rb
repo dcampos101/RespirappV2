56d1e70b377974841ef514b0329e8e772658a1f1
Denis Campos <dcneo03@gmail.com>
2018-10-25 09:02:43 -0500

    Av5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_option = {host:'https://aqueous-thicket-67866.herokuapp.com/'}
+  #config.action_mailer.default_url_option = {host:'https://aqueous-thicket-67866.herokuapp.com/'}
   
 end
